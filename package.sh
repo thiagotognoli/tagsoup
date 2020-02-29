4e78f0a151e8ab1f24ee0899b6bdf02798ecc64d
Thiago Tognoli <thiagotognoli@gmail.com>
2020-02-29 12:19:59 -0300

    tagsoup build incomplete

--- package.sh
+++ package.sh
@@ -2,7 +2,15 @@
 
 #docker run -it --rm --volume "$(pwd)/pom.xml"://usr/src/app/pom.xml maven:3-jdk-8-alpine mvn install
 
-docker run -it --rm --name tagsoup --volume "$(pwd)"/.m2:/root/.m2 -v "$(pwd)":/usr/src/tagsoup -w /usr/src/tagsoup maven:3.3-jdk-8 mvn clean package
+docker run -it --rm --name tagsoup \
+        --volume "$(pwd)"/.m2:/root/.m2 \
+        -v "$(pwd)":/usr/src/tagsoup \
+        -w /usr/src/tagsoup maven:3.3-jdk-8 \
+        mvn clean compile assembly:single
+
+mkdir -p dist \
+    && [ -e dist/tagsoup.jar ] && rm dist/tagsoup.jar || true \
+    && cp target/tagsoup-*.jar dist/tagsoup.jar
 
 #--volume "$(pwd)"/.m2:/root/.m2
 #-u $(id -u):$(id -g)
